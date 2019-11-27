23cb8daa8d16d42bb39d6a431c18d9648c4fd373
Prasanjit Singh <mailprasanjit@gmail.com>
2019-11-27 16:58:22 +0400

    Updated Kafka advertised IP

--- start-kafka-singlenode.sh
+++ start-kafka-singlenode.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 internal_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-export DOCKER_HOST_IP=$internal_ip
+external_ip=`curl -s icanhazip.com`
+export DOCKER_HOST_IP=$external_ip
 echo "Server IP is $DOCKER_HOST_IP"
-docker-compose -f zk-single-kafka-single.yml up
+docker-compose -f zk-single-kafka-single.yml up -d
 echo "Zookeeper will be available at $DOCKER_HOST_IP:2181"
 echo "Kafka will be available at $DOCKER_HOST_IP:9092"
 exit 0
